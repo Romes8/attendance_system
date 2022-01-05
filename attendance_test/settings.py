8ef2a25d4afd675e188943a864e86faef6733e8b
Roman <58208649+Romes8@users.noreply.github.com>
2022-01-05 09:58:37 +0100

    added IP address

--- attendance_test/settings.py
+++ attendance_test/settings.py
@@ -30,7 +30,7 @@ IPRESTRICT_GEOIP_ENABLED = False
 ALLOWED_HOSTS = ['kea-attendance.herokuapp.com', '127.0.0.1']
 
 LOGIN_URL = '/login/'
-ALLOWED_IP_BLOCKS = ['85.24.74.237', '94.18.243.162']
+ALLOWED_IP_BLOCKS = ['85.24.74.237', '94.18.243.162', '192.168.1.113']
 
 # Application definition
 
